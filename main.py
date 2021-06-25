7280c0ad7e9fcba0e918a5ff34837ee19a9961f2
Tomislav <tomislav.stanceric@gmail.com>
2021-06-25 15:34:50 +0200

    added: export STEAM games to JSON file, export steam level and badges, export recently played games

--- main.py
+++ main.py
@@ -4,15 +4,57 @@ import csv
 import os
 os.getcwd()
 
-import sys
-
 if __name__ == '__main__':
 
-    response = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v1?key=49FE5495F87DA18C0AAD77FC98F79032&steamid=76561198116938509&format=json&include_appinfo=True')
-    # response = requests.get('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1?key=49FE5495F87DA18C0AAD77FC98F79032&steamid=76561198116938509&format=json')
-    with open('game_data.json', 'w', encoding='utf-8') as jsonfile:
-        jsondata = json.dump(response.json(), jsonfile, ensure_ascii=False, indent=4)
-    # jsonData =
+    # To return owned games in STEAM form the URL with the following strings
+    steamLink = 'http://api.steampowered.com/'
+    ownedGames = 'IPlayerService/GetOwnedGames/v1?'
+    steamKey = '&key=49FE5495F87DA18C0AAD77FC98F79032'
+    steamID = '&steamid=76561198116938509'
+    textFormat = '&format=json'
+    appInfo ='&include_appinfo=True'
+    freeGames ='&include_played_free_games=True'
+
+    # Return owned STEAM games in JSON
+    getOwnedGames = requests.get(steamLink
+                             + ownedGames
+                             + steamKey
+                             + steamID
+                             + textFormat
+                             + appInfo
+                             + freeGames)
+
+    # Export in JSON file
+    with open('owned_games_steam_data.json', 'w', encoding='utf-8') as jsonfile:
+        json.dump(getOwnedGames.json(), jsonfile, ensure_ascii=False, indent=4)
+
+
+    # # Return reviews of owned STEAM games in JSON
+    # response2 = requests.get('https://store.steampowered.com/appreviews/252950?json=1&language=all')
+
+
+    # OPTIONAL: Get the STEAM level and badge level of the user
+    steamLevel ='IPlayerService/GetSteamLevel/v1?'
+    badgeLevel ='IPlayerService/GetBadges/v1?'
+
+    getSteamLevel = requests.get(steamLink + steamLevel + steamID + steamKey)
+    getBadgeLevel = requests.get(steamLink + badgeLevel + steamID + steamKey)
+
+    # Export in JSON file
+    with open('user_level_steam_data.json', 'w', encoding='utf-8') as jsonfile:
+        json.dump([getSteamLevel.json(),getBadgeLevel.json()], jsonfile, ensure_ascii=False, indent=4)
+
+
+    # OPTIONAL: Get recently played STEAM games
+    recentlyPlayed = 'IPlayerService/GetRecentlyPlayedGames/v1?'
+
+    getRecentlyPlayed = requests.get(steamLink + recentlyPlayed + steamID + steamKey)
+
+    # Export in JSON file
+    with open('recently_played_steam_data.json', 'w', encoding='utf-8') as jsonfile:
+        json.dump(getRecentlyPlayed.json(), jsonfile, ensure_ascii=False, indent=4)
+
+
 
     # with open('data.json') as jsonfile:
     #     jsonData = json.load(jsonfile)
