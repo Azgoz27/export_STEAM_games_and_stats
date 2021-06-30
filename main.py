4875a4200b7aa2631d202e9e8c07f6eaf8779f4d
Tomislav <tomislav.stanceric@gmail.com>
2021-06-30 17:23:06 +0200

    exporting game title, id, total playtime and HLTB times together all in one JSON file

--- main.py
+++ main.py
@@ -23,8 +23,21 @@ if __name__ == '__main__':
     freeGames ='&include_played_free_games=True'
 
     # Return owned STEAM games list
-    getOwnedGames = requests.get(steamLink + ownedGames + steamKey + steamID + textFormat + appInfo + freeGames)
-    exportJSON(getOwnedGames.json(), name='owned_games_steam')
+    getOwnedGames = (requests.get(steamLink + ownedGames + steamKey + steamID + textFormat + appInfo + freeGames)).json()
+    exportJSON(getOwnedGames, name='owned_games_steam')
+
+    # Scrap expected game times per play style from How Long to Beat website
+    howLongToBeat = HtmlScraper().search(name="Sid Meier's Civilization VI")
+    gameLengthList = []
+    for entry in howLongToBeat:
+        gameLengths = [entry.gameplayMain, entry.gameplayMainExtra, entry.gameplayCompletionist]
+        gameLengthTotal = dict(zip(entry.timeLabels, gameLengths))
+        hoursPerTitle = [entry.gameName, gameLengthTotal]
+        gameLengthList.append(hoursPerTitle)
+        # print(hoursPerTitle)
+
+        with open('HowLongToBeat.txt', 'w', encoding='utf-8') as textfile:
+            textfile.write(str(gameLengthList))
 
     # OPTIONAL: Get the STEAM level and badge level of the user
     steamLevel ='IPlayerService/GetSteamLevel/v1?'
@@ -40,36 +53,50 @@ if __name__ == '__main__':
     getRecentlyPlayed = requests.get(steamLink + recentlyPlayed + steamID + steamKey)
     exportJSON(getRecentlyPlayed.json(), name='recently_played_steam')
 
+    #OPTIONAL: Get game's Steam page details
+    gamePageID = '289070'
+
+    getGameDetails = requests.get('https://store.steampowered.com/api/appdetails?appids=' + gamePageID)
+    exportJSON(getGameDetails.json(), name='game_details')
+
+
+
     # Return review numbers of owned STEAM games
-    gameID = '289070'
-
-    getGameReview = requests.get('https://store.steampowered.com/appreviews/' + gameID +\
-                                 '?json=1&language=all&purchase_type=all')     # &review_type=positive
-    exportJSON(getGameReview.json(), name='game_review')
-
-    # Scrap expected game times per play style from How Long to Beat website
-    howLongToBeat = HtmlScraper().search(name="Sid Meier's Civilization VI")
-    gameLengthList = []
-    for entry in howLongToBeat:
-        gameLengths = [entry.gameplayMain, entry.gameplayMainExtra, entry.gameplayCompletionist]
-        gameLengthTotal = dict(zip(entry.timeLabels, gameLengths))
-        hoursPerTitle = [entry.gameName, gameLengthTotal]
-        gameLengthList.append(hoursPerTitle)
-        print(hoursPerTitle)
-
-        with open('HowLongToBeat.txt', 'w', encoding='utf-8') as textfile:
-            textfile.write(str(gameLengthList))
+    def getGameReviews(gameID):
+        gameReviews = (requests.get('https://store.steampowered.com/appreviews/'
+                            + gameID
+                            + '?json=1&language=all&purchase_type=all').json())         # &review_type=positive
+        return(' Total Votes: ' + str(gameReviews['query_summary']['total_reviews'])
+               + ' Total Positive: ' + str(gameReviews['query_summary']['total_positive'])
+               + ' Total Negative: ' + str(gameReviews['query_summary']['total_negative'])
+               )
 
 
+    # Get the total count of Steam games
+    gamesTotal = "Total Steam game count: " + str(getOwnedGames['response']['game_count'])
+    # Get the game IDs, names, total playtime per title, total vote numbers, total positive and negative numbers
+    gameItems = []
+
+    for item in getOwnedGames['response']['games']:
+        gameItems.append('ID: ' + str(item['appid'])
+                         + ' name: ' + str(item['name'])
+                         + ' Total Playtime: ' + str(item['playtime_forever'])
+                         + ' Total Votes: ' + str(getGameReviews(str(item['appid'])))
+                         )
+        print(('ID: ' + str(item['appid'])
+                         + ' name: ' + str(item['name'])
+                         + ' Total Playtime: ' + str(item['playtime_forever'])
+                         + ' Total Votes: ' + str(getGameReviews(str(item['appid'])))
+                         ))
+
+
+    # Get the game's total votes number
+    exportJSON((gamesTotal, gameItems), name='game_list')
 
 
 
-    # getGameDetails = requests.get('https://store.steampowered.com/api/appdetails?appids=' + gameID)
 
 
-    # with open('data.json') as jsonfile:
-    #     jsonData = json.load(jsonfile)
-    #
     # dataFileCsv = open('data.csv', 'w', newline='')
     # csvWriter = csv.writer(dataFileCsv)
     #
