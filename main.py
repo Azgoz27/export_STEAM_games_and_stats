51cdd6a9b8cd6af704060de506194d8c3879ce82
Tomislav <tomislav.stanceric@gmail.com>
2021-07-01 15:56:53 +0200

    exporting game number, title, id, steam playtime and HLTB times, score and steam db score, all to JSON file. need to work on HLTB better implementation

--- main.py
+++ main.py
@@ -2,14 +2,41 @@ import requests
 import json
 import csv
 import os
+import math
 from hltbapi import HtmlScraper
 os.getcwd()
 
 if __name__ == '__main__':
 
+    # Export file to JSON
     def exportJSON(file, name):
         with open(name + '_data.json', 'w', encoding='utf-8') as jsonfile:
             json.dump(file, jsonfile, ensure_ascii=False, indent=4)
+
+    # Return review numbers of owned STEAM games
+    def getGameReviews(gameID):
+        gameReviews = (requests.get('https://store.steampowered.com/appreviews/'
+                            + gameID
+                            + '?json=1&language=all&purchase_type=all').json())         # &review_type=positive
+        totalVotes = gameReviews['query_summary']['total_reviews']
+        totalPositive = gameReviews['query_summary']['total_positive']
+        totalNegative = gameReviews['query_summary']['total_negative']
+        reviewScore = totalPositive/totalVotes
+        rating = reviewScore-(reviewScore-0.5) * math.pow(2, - math.log10(totalVotes + 1))
+        return(' Total Votes: ' + str(totalVotes) + ', '
+               + ' Total Positive: ' + str(totalPositive) + ', '
+               + ' Total Negative: ' + str(totalNegative) + ', '
+               + ' Steam Score(%): ' + str(round(reviewScore*100)) + ', '
+               + ' Steam db Score(%): ' + str(round(rating*100))
+               )
+
+    # Scrap expected game length times per play style from How Long to Beat website
+    def getHowLongToBeat(name):
+        howLongToBeat = HtmlScraper().search(name)[0]
+        gameLengths = 'Main Story(h): ' + str(howLongToBeat.gameplayMain) + ', '\
+                      + 'Extra(h): ' + str(howLongToBeat.gameplayMainExtra) + ', '\
+                      + 'Complete(h): ' + str(howLongToBeat.gameplayCompletionist) + ', '
+        return str(gameLengths)
 
     # To return owned games in STEAM form the URL with the following strings
     steamLink = 'http://api.steampowered.com/'
@@ -23,21 +50,32 @@ if __name__ == '__main__':
     freeGames ='&include_played_free_games=True'
 
     # Return owned STEAM games list
-    getOwnedGames = (requests.get(steamLink + ownedGames + steamKey + steamID + textFormat + appInfo + freeGames)).json()
+    getOwnedGames = (requests.get(steamLink + ownedGames + steamKey + steamID + textFormat + appInfo
+                                  + freeGames)).json()
     exportJSON(getOwnedGames, name='owned_games_steam')
 
-    # Scrap expected game times per play style from How Long to Beat website
-    howLongToBeat = HtmlScraper().search(name="Sid Meier's Civilization VI")
-    gameLengthList = []
-    for entry in howLongToBeat:
-        gameLengths = [entry.gameplayMain, entry.gameplayMainExtra, entry.gameplayCompletionist]
-        gameLengthTotal = dict(zip(entry.timeLabels, gameLengths))
-        hoursPerTitle = [entry.gameName, gameLengthTotal]
-        gameLengthList.append(hoursPerTitle)
-        # print(hoursPerTitle)
 
-        with open('HowLongToBeat.txt', 'w', encoding='utf-8') as textfile:
-            textfile.write(str(gameLengthList))
+    # Get the total count of Steam games
+    gamesTotal = "Total Steam game count: " + str(getOwnedGames['response']['game_count'])
+    # Get the game IDs, names, total playtime per title, total vote numbers, total positive and negative numbers
+    gameItems = []
+
+    for count, item in enumerate(getOwnedGames['response']['games'], 1):
+        gameItems.append(str(count) + ', '
+                         + 'ID: ' + str(item['appid']) + ', '
+                         + ' name: ' + str(item['name']) + ', '
+                         + ' Steam Playtime(h): ' + str(item['playtime_forever']) + ', '
+                         + getHowLongToBeat(name=(str(item['name'])))
+                         + str(getGameReviews(str(item['appid'])))
+                         )
+
+        print(gameItems)
+
+
+    # Export to JSON file
+    exportJSON((gamesTotal, gameItems), name='game_list')
+
+
 
     # OPTIONAL: Get the STEAM level and badge level of the user
     steamLevel ='IPlayerService/GetSteamLevel/v1?'
@@ -54,45 +92,9 @@ if __name__ == '__main__':
     exportJSON(getRecentlyPlayed.json(), name='recently_played_steam')
 
     #OPTIONAL: Get game's Steam page details
-    gamePageID = '289070'
-
+    gamePageID = '12370'
     getGameDetails = requests.get('https://store.steampowered.com/api/appdetails?appids=' + gamePageID)
     exportJSON(getGameDetails.json(), name='game_details')
-
-
-
-    # Return review numbers of owned STEAM games
-    def getGameReviews(gameID):
-        gameReviews = (requests.get('https://store.steampowered.com/appreviews/'
-                            + gameID
-                            + '?json=1&language=all&purchase_type=all').json())         # &review_type=positive
-        return(' Total Votes: ' + str(gameReviews['query_summary']['total_reviews'])
-               + ' Total Positive: ' + str(gameReviews['query_summary']['total_positive'])
-               + ' Total Negative: ' + str(gameReviews['query_summary']['total_negative'])
-               )
-
-
-    # Get the total count of Steam games
-    gamesTotal = "Total Steam game count: " + str(getOwnedGames['response']['game_count'])
-    # Get the game IDs, names, total playtime per title, total vote numbers, total positive and negative numbers
-    gameItems = []
-
-    for item in getOwnedGames['response']['games']:
-        gameItems.append('ID: ' + str(item['appid'])
-                         + ' name: ' + str(item['name'])
-                         + ' Total Playtime: ' + str(item['playtime_forever'])
-                         + ' Total Votes: ' + str(getGameReviews(str(item['appid'])))
-                         )
-        print(('ID: ' + str(item['appid'])
-                         + ' name: ' + str(item['name'])
-                         + ' Total Playtime: ' + str(item['playtime_forever'])
-                         + ' Total Votes: ' + str(getGameReviews(str(item['appid'])))
-                         ))
-
-
-    # Get the game's total votes number
-    exportJSON((gamesTotal, gameItems), name='game_list')
-
 
 
 
